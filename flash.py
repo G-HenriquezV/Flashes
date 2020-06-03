713d8d719cf88e9df45a79545ce14b6caa7e9f50
German Henriquez <germanandres.hv@gmail.com>
2020-06-03 17:31:14 -0400

    Removed unnecesary code

--- flash.py
+++ flash.py
@@ -11,7 +11,20 @@ Base = declarative_base()
 
 class Flashcard(Base):
     """
-    TODO: Write this
+    Flashcard type object. Contains a title, a content (what's written on it) and a date when it was
+    created. Aditionally, the flashcard can be marked as done.
+
+    Properties:
+        id: int
+            Unique identifier of the flashcard on the database
+        title: str
+            Title of the flashcard
+        content: str
+            Content of the flashcard
+        created_at: datetime
+            When the flashcard was written
+        is_checked: bool
+            If the flashcard is marked as done
     """
     __tablename__ = 'flashcards'
     id = Column(Integer, primary_key=True)
