458c747b6b992582ecd5d54f20c3a4e39af2f3a2
German Henriquez <germanandres.hv@gmail.com>
2020-06-03 17:25:12 -0400

    Removed unnecesary code

--- flash.py
+++ flash.py
@@ -20,24 +20,7 @@ class Flashcard(Base):
     created_at = Column(DateTime, default=datetime.now)
     is_checked = Column(Boolean)
 
-    # def __init__(self, title: str, content: str, created_at: Optional[datetime] = None):
-    #     self.title = title
-    #     self.content = content
-    #     if created_at is None:
-    #         created_at = datetime.today()
-    #     self.created_at = created_at
-    #     self.is_checked = False
-
     def __str__(self):
         ttl = f'{self.id} - {self.title} {"(Done)" if self.is_checked else ""}'
         date = self.created_at.strftime('%b %d %Y')
         return f'{ttl}\n{date}\n\n{self.content}'
-
-    # def check(self):
-    #     """
-    #     Changes between checked and unchecked
-    #     """
-    #     if self.is_checked:
-    #         self.is_checked = False
-    #     else:
-    #         self.is_checked = True
